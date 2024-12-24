f37354e0ec73a06026073fec9a7301a7ac0fd84f
ruli sastra putri <ruli@accelbyte.net>
2024-12-24 11:28:39 +0700

    chore(demo): update display name for validation error

--- src/AccelByte.PluginArch.SessionDsm.Demo.Client/Model/NewTestPlayer.cs
+++ src/AccelByte.PluginArch.SessionDsm.Demo.Client/Model/NewTestPlayer.cs
@@ -64,7 +64,8 @@ namespace AccelByte.Sdk.Tests.Mod.Scenario
                 DisplayName = $"Server SDK Test Player {GenerateRandomNumber(3)}",
                 Username = _UserName,
                 Country = "ID",
-                DateOfBirth = "1995-01-10"
+                DateOfBirth = "1995-01-10",
+                UniqueDisplayName = _UserName
             };
 
             AccountCreateUserResponseV4? cuResp = _AdminSdk.Iam.UsersV4.PublicCreateUserV4Op
@@ -80,7 +81,7 @@ namespace AccelByte.Sdk.Tests.Mod.Scenario
                 .EnableLog()
                 .Build();
 
-            _Sdk.LoginUser(_UserName, user_password);
+            _Sdk.LoginUser(user_email, user_password);
         }
 
         public void Login() { }
